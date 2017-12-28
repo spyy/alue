bb65b8e0421cbd3baad4393271561b38b105542f
spyy <sami.pyy@gmail.com>
2017-12-28 11:56:37 +0200

    ...

--- js/storage.js
+++ js/storage.js
@@ -1,5 +1,5 @@
 const storageVersion = '1.';
-const doorItem = {
+const doorModel= {
     number: 0,
     name: '',
     description: '',
@@ -8,7 +8,7 @@ const doorItem = {
     time: '',
     color: ''
 };
-var areaItem = {
+const areaModel = {
     key: '',
     name: '',
     from: 1,
@@ -23,152 +23,83 @@ var areaItem = {
 
 
 function createArea(name, from, to) {
-    areaItem.key = storageVersion + Date.now().toString();
-    areaItem.name = name;
-    areaItem.from = from;
-    areaItem.to = to;
-    areaItem.createdAt = new Date().toUTCString();
-    
-    for (var i = Number(from); i <= Number(to); i++) {
-        doorItem.number = i;
-        areaItem.doors.push( { number: i } );
-    }
-    
-    window.localStorage.setItem(areaItem.key, JSON.stringify(areaItem));
-    
-    return areaItem;
-}
+    var area = Object.assign({}, areaModel);
+    area.key = storageVersion + Date.now().toString();
+    area.name = name;
+    area.from = from;
+    area.to = to;
+    area.createdAt = new Date().toUTCString();
 
+    for (var i = Number(from); i <= Number(to); i++) {
+        var door = Object.assign({}, doorModel);
+        door.number = i;
+        area.doors.push(door);
+    }
+
+    window.localStorage.setItem(area.key, JSON.stringify(area));
+
+    return area;
+}
 
 function removeArea(key) {
     console.log('removeArea: ' + key);
     window.localStorage.removeItem(key);
 }
 
-
-function getAreaNameDeprecated(key) {
-    return window.localStorage.getItem(key);
-}
-
-
-function getAreaFromDeprecated(area) {
-    var key = area + '_from'
-    return window.localStorage.getItem(key);
-}
-
-
-function getAreaToDeprecated(area) {
-    var key = area + '_to'
-    return window.localStorage.getItem(key);
-}
-
-
 function getWeekDay() {
     const WEEK_DAYS = ['SU', 'MA', 'TI', 'KE', 'TO', 'PE', 'LA'];
     var d = new Date();
-    
+
     return WEEK_DAYS[d.getDay()];
 }
-
 
 function getDate() {
     var d = new Date();
     var day = d.getDate();
     var month = d.getMonth() + 1;
-    
+
     return day + '.' + month;
 }
-
 
 function getTime() {
     var d = new Date();
     var hours = d.getHours();
     var minutes = d.getMinutes();
     minutes = minutes > 9 ? minutes : '0' + minutes;
-    
+
     return hours + ':' + minutes;
 }
-
 
 function getDateAndTime() {
     return getDate() + ' ' + getTime();
 }
 
-
-function getAreasDeprecated() {            
-    var res = [];
-    
-    var i;
-    for (i = 0; i < window.localStorage.length; i++) {
-        var key = window.localStorage.key(i);
-        
-        if (key.search(/[._]/g) === -1) {
-            console.log('vanha: ' + key);
-            res.push(key);
-        }
-    }
-    
-    return res;
-}
-
-
-function getDoorsDeprecated(area) {            
-    var res = [];
-    
-    var i;
-    for (i = 0; i < window.localStorage.length; i++) {
-        var key = window.localStorage.key(i);
-        
-        if (key.search('_') === -1) {
-            res.push(key);
-        }
-    }
-    
-    return res;
-}
-
-
 function setSelectedArea(area) {
     window.sessionStorage.selectedArea = area;
 }
-
 
 function getSelectedArea() {
     return window.sessionStorage.selectedArea;
 }
 
-
-function setDoorDeprecated(area, door, color) {
-    var key = area + '__' + door;
-    var value = color + ' ' + getDateAndTime() + ' ' + getWeekDay();
-    
-    window.localStorage.setItem(key, value);
-}
-
-
-function getDoorDeprecated(area, door) {
-    var key = area + '__' + door;
-    return window.localStorage.getItem(key);
-}
-
-function getAreas() {            
+function getAreas() {
     var res = [];
-    
+
     var i;
     for (i = 0; i < window.localStorage.length; i++) {
         var key = window.localStorage.key(i);
-        
+
         if (key.search(/[.]/g) !== -1) {
             var item = window.localStorage.getItem(key);
             console.log('uusi: ' + item);
             res.push(JSON.parse(item));
         }
     }
-    
+
     return res;
 }
 
-function getArea(key) {            
+function getArea(key) {
     return JSON.parse(window.localStorage.getItem(key));
 }
 
@@ -176,17 +107,32 @@ function setArea(area) {
     window.localStorage.setItem(area.key, JSON.stringify(area));
 }
 
-function setDoor(key, number, color) {
+function getDoor(key, number) {
     var area = getArea(key);
-    
+    var res = null;
+
     area.doors.forEach(function(door) {
-        if (door.number === number ) {
+        if (door.number == number) {
+          res = door;
+        }
+    });
+
+    return res;
+}
+
+function setDoor(key, number, color, description) {
+    var area = getArea(key);
+
+    area.doors.forEach(function(door) {
+        if (door.number == number ) {
             door.color = color;
+            door.name = description;
+            door.description = description;
             door.date = getDate();
             door.weekday = getWeekDay();
             door.time = getTime();
         }
     });
-    
+
     setArea(area);
 }
